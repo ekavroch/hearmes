b621df10bfca8a51559349b7be8e0ec78905a661
Boaz Keren Gil <boazkg@icloud.com>
2018-03-09 17:49:17 +0100

    Addeed view func

--- hearmes_app/views.py
+++ hearmes_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.loader import get_template
+from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 import requests
 import datetime
 import json
+from django.http import JsonResponse
 from hearmes_app import models
 
 # Create your views here.
@@ -17,14 +19,22 @@ class RegisterView(CreateView):
 
 def registerRefugee(request):
     #Prepare data:
-    first_name = request.GET.get('first_name', None)
-    last_name = request.GET.get('last_name', None)
-    destination = request.GET.get('destination', None)
-    age = requests.GET.get('age', None)
-    story = requests.GET.get('story', None)
-    job = requests.GET.get('job', None)
-    tags = requests.GET.get('tags', None)
+    print(request.POST.get('first_name'))
+    first_name = request.POST.get('first_name')
+    last_name = request.POST.get('last_name')
+    destination = request.POST.get('destination')
+    age = request.POST.get('age')
+    job = request.POST.get('job')
+    tags = request.POST.get('tags')
     date = datetime.datetime.now()
+    story_url = ""
+
+    #Upload the story
+    if request.method == 'POST' and request.FILES['story']:
+        myfile = request.FILES['story']
+        fs = FileSystemStorage()
+        filename = fs.save('stories/'+first_name + '_story.jpg', myfile)
+        story_url = fs.url(filename)
 
     #JSON:
     refugee = {
@@ -32,15 +42,23 @@ def registerRefugee(request):
         "last_name":last_name,
         "destination":destination,
         "age":age,
-        "story":story,
+        "story":story_url,
         "job":job,
         "tags":tags,
         "date":date
     }
 
+
     #Send to DB:
-    res = models.uploadFormToDB(refugee)
-    return res
+    savedRefugee = models.uploadFormToDB(refugee)
+    text = models.JPEGtoText(story_url)
+    model.updateDB(savedRefugee.id,"story_text", text)
+
+    data = {
+        'res': 200,
+    }
+
+    return JsonResponse(data)
 
 
 
