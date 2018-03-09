d12cf218c551b71800af9178fbceb91e3abe71b2
Boaz Keren Gil <boazkg@icloud.com>
2018-03-09 12:58:34 +0100

    Added SCSS and Typescript

--- hearmes_app/models.py
+++ hearmes_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+##TODO: Model to upload document & details
+
+##TODO: Model for transalation
+
+##TODO: Model for sentiment and keywords detection
+
+##TODO: Model to get 2 sentences
+
+##TODO: Model to upload to database
+
+##TODO: Model to retrive from database based on tags
--- hearmes_app/views.py
+++ hearmes_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+##TODO: Create view of an article
+
+##TODO: Create view of 2 sentences
+
+##TODO: Create view of registration page
