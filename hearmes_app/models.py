604e4bd5a8b9eb14d1bfc0694c9d771ac5339712
Boaz Keren Gil <boazkg@icloud.com>
2018-03-09 13:43:28 +0100

    Added VM Control

--- hearmes_app/models.py
+++ hearmes_app/models.py
@@ -1,15 +1,37 @@
 from django.db import models
+import requests
+from pprint import pprint
+
+#AZURE STUFF:
+subscription_key="dac80f6bdc41438dbb9dcc1c9a81b3e1"
+assert subscription_key
+text_analytics_base_url = "https://westeurope.api.cognitive.microsoft.com/text/analytics/v2.0/"
+language_api_url = text_analytics_base_url + "languages"
+key_phrase_api_url = text_analytics_base_url + "keyPhrases"
+sentiment_api_url = text_analytics_base_url + "sentiment"
+
+
+
 
 # Create your models here.
 
-##TODO: Model to upload document & details
+#TODO: Model to upload document & details
 
-##TODO: Model for transalation
+#TODO: Model to change JPG to Computer Text
 
-##TODO: Model for sentiment and keywords detection
+#TODO: Model for transalation
 
-##TODO: Model to get 2 sentences
+#Model for sentiment and keywords detection
+def analyzeDoc(doc):
+    headers = {"Ocp-Apim-Subscription-Key": subscription_key}
+    sentiment_phrases_response = requests.post(sentiment_api_url, headers=headers, json=doc)
+    key_phrases_response = requests.post(key_phrase_api_url, headers=headers, json=doc)
+    key_phrases = key_phrases_response.json()
+    sentiment_phrases = key_phrases_response.json()
 
-##TODO: Model to upload to database
 
-##TODO: Model to retrive from database based on tags
+#TODO: Model to get 2 sentences
+
+#TODO: Model to upload to database
+
+#TODO: Model to retrive from database based on tags
--- hearmes_app/views.py
+++ hearmes_app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import get_template
+import datetime
 
 # Create your views here.
 ##TODO: Create view of an article
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    t = get_template('test.html')
+    html = t.render({'current_date': now})
+    return HttpResponse(html)
 
 ##TODO: Create view of 2 sentences
 
