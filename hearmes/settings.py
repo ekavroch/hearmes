47f0801352ee6dd8b6d7828de7f45253703445e2
Evi Kavrochorianou <evangelia.a.kavrochorianou@gmail.com>
2018-03-09 22:24:06 +0200

    offline bootstrap

--- hearmes/settings.py
+++ hearmes/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hearmes_app.apps.HearmesAppConfig',
+    'hearmes_app',
 ]
 
 MIDDLEWARE = [
@@ -119,5 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- hearmes_app/models.py
+++ hearmes_app/models.py
@@ -7,27 +7,34 @@ import urllib.parse
 # Create your models here.
 #TODO: Model to upload document & details: Name, Birthdate, Document
 class Migrant(models.Model):
-    migrant_id = models.CharField(max_length=30,primary_key=True)
+    migrant_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    destination = models.CharField(max_length=30)
     age = models.CharField(max_length=3)
-    profession = models.CharField(max_length=30)
-    message = models.CharField(max_length=255, null=True)
+    job = models.CharField(max_length=30)
+    message_text = models.CharField(max_length=255, null=True)
     message_img_path = models.CharField(max_length=255, null=True)
-    anonymity = models.CharField(max_length=30)
+    tags = models.CharField(max_length=255, null=True)
+    date = models.CharField(max_length=30, null=True)
+    anonymity = models.CharField(max_length=30, null=True)
 
 def uploadFormToDB(form_details):
 
     dump = json.dumps(form_details)
     data = json.loads(dump, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
 
-    p = Migrant(first_name=data.first_name, last_name=data.last_name, age = data.age , profession = data.profession, message = data.message, message_img = data.message_img, anonymity = data.anonymity)
-    p.save()
+    try:
+        # p = Migrant(first_name="Daren", last_name="Tan", age="22", profession="Data Scientist", message_img_path="hehe", anonymity="false")
+        p = Migrant(first_name=data.first_name, last_name=data.last_name, destination = data.destination, age=data.age, job=data.job,
+                    message_img_path=data.story_url, tags=data.tags, data=data.date)
+        p.save()
+    except Exception as e:
+        return e
 
-    m = Migrant.objects.all()
-    print(m)
+    m = Migrant.objects.latest('migrant_id')
 
-    return 200
+    return m
 
 #TODO: Model to change JPG to Computer Text
 def JPEGtoText():
@@ -54,11 +61,14 @@ def JPEGtoText():
         analysis = response_final.json()
         time.sleep(1)
 
+    full_text = ""
     for line in analysis["recognitionResult"]["lines"]:
-        print(line['text'])
+        full_text += line['text']
+
+    return full_text
 
 #Model for transalation
-def transalte (target, text):
+def translate (target, text):
     payload = {'to': target, 'text': urllib.parse.quote(text)}
     headers = {'Ocp-Apim-Subscription-Key': 'b0e5aa9d010346e2954b4ff20087b6dd'}
     response = requests.get('https://api.microsofttranslator.com/V2/Http.svc/Translate', params=payload, headers=headers)
@@ -82,5 +92,10 @@ def analyzeStory(story):
 #TODO: Model to get 2 sentences
 
 #TODO: Model to upload to database
+def updateDB(id, field, value):
+
+    m = Migrant.objects.filter(pk=id)
+    setattr(m, field, value)  # f.foo=bar
+    m.save()
 
 #TODO: Model to retrive from database based on tags
